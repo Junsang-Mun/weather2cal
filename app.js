4414465106a301eab7b3233b60ef631813a5a5fd
Junsang-mun <mun.js@aol.com>
2022-07-20 16:18:57 +0900

    Add; app.js; api 콜 할 수 있도록 requrl 구현하는 로직 작성
    - 현재 날짜 및 시간 받아와서 최신의 예보 시간으로 맞추어 request url 생성하도록 함수 작성

--- app.js
+++ app.js
@@ -1,8 +1,32 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+require("dotenv").config();
+const today = new Date();
 
-fetch('')
+function giveMeBaseDate() {
+    const year = today.getFullYear();
+    const month = ('0' + (today.getMonth() + 1)).slice(-2);
+    const day = today.getDate();
+    return (year + month + day);
+}
 
-//app.get('/', (req, res) => res.send('Hello World!'));
-//app.listen(port, () => console.log('Listening...'));
+function giveMeBaseTime() {
+    const forecastTime = [2, 5, 8, 11, 14, 17, 20, 23];
+    const ref = today.getHours();
+    let time = 0;
+    
+    for (let i = 0; i < forecastTime.length; i ++) {
+        if (forecastTime[i] <= ref) {
+            if (forecastTime[i + 1] > ref) {
+                time = forecastTime[i];
+            }
+        }
+    }
+    time = ('0' + time).slice(-2);
+    return (time + '00');
+}
+
+function giveMeReqUrl() {
+    let time = giveMeBaseTime();
+    let date = giveMeBaseDate();
+    return ('https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst?serviceKey='+process.env.WEATHER_API_KEY+'&pageNo=1&numOfRows=20&dataType=JSON&base_date='+date+'&base_time='+time+'&nx=55&ny=127');
+}
+
