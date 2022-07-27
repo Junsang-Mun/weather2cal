b0b60630d8452568ace418e9e27692f3a784eb03
Junsang-mun <mun.js@aol.com>
2022-07-27 11:03:31 +0900

    Add; sky, pty 값 NL로 변환
    toNL 함수 모두 적용

--- app.js
+++ app.js
@@ -49,6 +49,32 @@ function giveMeReqUrl() {
     return ('http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst?serviceKey='+process.env.WEATHER_API_KEY+'&pageNo=1&numOfRows=48&dataType=JSON&base_date='+date+'&base_time='+time+'&nx=61&ny=126');
 }
 
+function toNL(type, val) {
+    if (type === 'SKY') {
+        switch (val) {
+            case '1':
+                return '맑음';
+            case '3':
+                return '구름 많음';
+            case '4':
+                return '흐림';
+        }
+    } else if (type === 'PTY') {
+        switch (val) {
+            case '0':
+                return '강수 없음';
+            case '1':
+                return '비';
+            case '2':
+                return '눈비';
+            case '3':
+                return '눈';
+            case '4':
+                return '소나기';
+        }
+    }
+}
+
 async function getWeather() {
     const reqUrl = giveMeReqUrl();
     let res = await axios.get(reqUrl);
@@ -57,37 +83,33 @@ async function getWeather() {
 
     base0.TIME = data.item[0].fcstTime;
     base0.TMP = data.item[0].fcstValue;
-    base0.SKY = data.item[5].fcstValue;
-    base0.PTY = data.item[6].fcstValue;
+    base0.SKY = toNL('SKY', data.item[5].fcstValue);
+    base0.PTY = toNL('PTY', data.item[6].fcstValue);
     base0.POP = data.item[7].fcstValue;
 
     base1.TIME = data.item[12].fcstTime;
     base1.TMP = data.item[12].fcstValue;
-    base1.SKY = data.item[17].fcstValue;
-    base1.PTY = data.item[18].fcstValue;
+    base1.SKY = toNL('SKY', data.item[17].fcstValue);
+    base1.PTY = toNL('PTY', data.item[18].fcstValue);
     base1.POP = data.item[19].fcstValue;
 
     base2.TIME = data.item[24].fcstTime;
     base2.TMP = data.item[24].fcstValue;
-    base2.SKY = data.item[29].fcstValue;
-    base2.PTY = data.item[30].fcstValue;
+    base2.SKY = toNL('SKY', data.item[29].fcstValue);
+    base2.PTY = toNL('PTY', data.item[30].fcstValue);
     base2.POP = data.item[31].fcstValue;
     
     base3.TIME = data.item[36].fcstTime;
     base3.TMP = data.item[36].fcstValue;
-    base3.SKY = data.item[41].fcstValue;
-    base3.PTY = data.item[42].fcstValue;
+    base3.SKY = toNL('SKY', data.item[41].fcstValue);
+    base3.PTY = toNL('PTY', data.item[42].fcstValue);
     base3.POP = data.item[43].fcstValue;
 
-    console.log(base0.TMP + '  ' + base0.TIME+ '  ' + base0.SKY+ '  ' + base0.PTY+ '  ' + base0.POP + '\n');
-    console.log(base1.TMP + '  ' + base1.TIME+ '  ' + base1.SKY+ '  ' + base1.PTY+ '  ' + base1.POP + '\n');
-    console.log(base2.TMP + '  ' + base2.TIME+ '  ' + base2.SKY+ '  ' + base2.PTY+ '  ' + base2.POP + '\n');
-    console.log(base3.TMP + '  ' + base3.TIME+ '  ' + base3.SKY+ '  ' + base3.PTY+ '  ' + base3.POP + '\n');
-    test()
+    mixfcst()
 }
 
 getWeather();
 
-function test() {
-    console.log(base0.TMP + 'asdfasdfasdfasdfasdfasdf');
+function mixfcst() {
+    console.log(base1.SKY + '     ' + base0.PTY);
 }
